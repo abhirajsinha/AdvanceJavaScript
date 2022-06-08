502338e3764aefbaa58082e4654831d3b552bd7c
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-06-08 06:33:42 +0530

    add

---
+++ imperativeVSdeclarative.js
@@ -0,0 +1,14 @@
+//iS the square of the number is even or not
+const x=5;
+
+const xSquared = x*x;
+
+let isEven;
+
+if(xSquared %2 === 0){
+    isEven=true;
+}else{
+    isEven=false; 
+}
+
+console.log("Given value is Even,",isEven);
