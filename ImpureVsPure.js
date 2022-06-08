1b109d2181e838772aa6915ed6c63ec0844aa594
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-06-08 06:50:05 +0530

    add

--- ImpureVsPure.js
+++ ImpureVsPure.js
@@ -1,19 +1,19 @@
 //Impure Functions
                     // Functions which returns different result when called everytime
-let a=7;
-function addNum(b){
-    console.log(`The Sum is`, a+b);
-}
+// let a=7;
+// function addNum(b){
+//     console.log(`The Sum is`, a+b);
+// }
 
-addNum(2);
+// addNum(2);
 
 
 //Pure Functions
                 // Functions which returns the same result when called with same set of arguments everytime
 
-// function addNum(a,b){
-//     console.log(`the sum is`, a+b);// Side Effect of Pure Function
-//     return a+b;
-// }
+function addNum(a,b){
+    // console.log(`the sum is`, a+b);// Side Effect of Pure Function
+    return a+b;
+}
 
-// addNum(2,3);
+addNum(2,3);
