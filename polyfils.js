cf839a7bbcebc6ccc04ec5a17b681ad3b708f85f
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-06-09 22:13:30 +0530

    Custom Filter Function

--- polyfils.js
+++ polyfils.js
@@ -1,4 +1,4 @@
-//Map
+//Map Function
 // let myArr = [1,2,3,4,5];
 
 // let mappedArr = myArr.map(function(x){
@@ -7,23 +7,58 @@
 
 // console.log(mappedArr);
 
-//Custome Map
-let myArr = [1,2,3,4,5];
+//Custome Map Function
+// let myArr = [1,2,3,4,5];
 
-function myPolyfils(myArr,cb){
-    let newArr=[];
+// function myPolyfils(myArr,cb){
+//     let newArr=[];
 
-    for(let i=0;i<myArr.length;i++){
-        newArr.push(cb(myArr[i]));
+//     for(let i=0;i<myArr.length;i++){
+//         newArr.push(cb(myArr[i]));
+//     }
+
+//     return newArr;
+// }
+
+
+// function square(x){
+//     return x*x;
+// }
+
+// console.log(myArr);
+// console.log(myPolyfils(myArr,square));
+
+
+
+//Filter Function
+// let fArr=[2,4,6,1,3,5];
+
+// let evenArr = fArr.filter(function(x){
+//     return x%2==0;
+// })
+
+// console.log(fArr);
+// console.log(evenArr);
+
+
+//Custom filter function
+let fArr=[2,4,6,1,3,5];
+function myPolyfilFilter(filterArr,cb){
+    let evenArr = [];
+
+    for(let i=0;i<filterArr.length;i++){
+        if(cb(filterArr[i])){
+            evenArr.push(filterArr[i]);
+        }
     }
 
-    return newArr;
+    return evenArr;
 }
 
-
-function square(x){
-    return x*x;
+function isEven(x){
+    if(x%2==0) return x;
 }
 
-console.log(myArr);
-console.log(myPolyfils(myArr,square));
+console.log(fArr);
+console.log(myPolyfilFilter(fArr,isEven));
+
