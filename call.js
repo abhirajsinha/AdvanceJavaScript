b1df86d95c32129a759dd757d1b7c5b33d8dddf2
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-06-12 12:34:03 +0530

    Pass Arguments

--- call.js
+++ call.js
@@ -21,6 +21,27 @@
 
 /`````````````/
 
+// let person1 = {
+//     name:'Aman',
+//     age: 25,
+// }
+
+// let person2 = {
+//     name:'Steve',
+//     age:20
+// }
+
+// let showDetails = function(){
+//         console.log(this.name + " is " + this.age+" years old");
+// }
+
+// showDetails.call(person1);
+// showDetails.call(person2);
+
+
+
+/// Pass Arguments
+
 let person1 = {
     name:'Aman',
     age: 25,
@@ -31,9 +52,8 @@ let person2 = {
     age:20
 }
 
-let showDetails = function(){
-        console.log(this.name + " is " + this.age+" years old");
+let showDetails = function(city, car){
+    console.log(`${this.name} is ${this.age} years old, he lives in ${city} and he drives ${car}`);
 }
 
-showDetails.call(person1);
-showDetails.call(person2);
+showDetails.call(person2, "Hyderabad", "Mercedes");
