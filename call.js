1c86481baa06caa5a98ff2cbfdad1dc222b34fdc
Abhiraj Sinha <abhirajsinha25@gmail.com>
2022-06-12 12:28:16 +0530

    Call Func for Multiple objects

--- call.js
+++ call.js
@@ -1,10 +1,29 @@
+// let person1 = {
+//     name:'Aman',
+//     age: 25,
+
+//     showDetails : function(){
+//         console.log(this.name + " is " + this.age+" years old");
+//     }
+// }
+
+// let person2 = {
+//     name:'Steve',
+//     age:20
+// }
+
+// person1.showDetails();
+
+// //call the showDetails func using person2 [function bowrrowing]
+// person1.showDetails.call(person2);
+
+
+
+/`````````````/
+
 let person1 = {
     name:'Aman',
     age: 25,
-
-    showDetails : function(){
-        console.log(this.name + " is " + this.age+" years old");
-    }
 }
 
 let person2 = {
@@ -12,7 +31,9 @@ let person2 = {
     age:20
 }
 
-person1.showDetails();
+let showDetails = function(){
+        console.log(this.name + " is " + this.age+" years old");
+}
 
-//call the showDetails func using person2 
-person1.showDetails.call(person2);
+showDetails.call(person1);
+showDetails.call(person2);
